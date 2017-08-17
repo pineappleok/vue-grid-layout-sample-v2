71893c91122f5d27aae6b79b826e957a7dba3e45
Gustavo Santos <gustavo.santos@jbaysolutions.com>
2017-08-17 15:47:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue-grid-layout-sample
+
+A [vue-grid-layout](https://github.com/jbaysolutions/vue-grid-layout) v2 usage sample.
+
+
+## Requirements
+
+* Node.js
+
+
+## Usage
+
+Install dependencies:
+
+    npm install
+    
+Run dev server (webpack):
+    
+    npm run dev
+
+Open browser at http://localhost:8080
---
+++ src/TestElement.vue
@@ -0,0 +1,22 @@
+<template>
+    <span class="text">
+        {{text}}
+    </span>
+</template>
+<style>
+</style>
+<script>
+    export default {
+        name: "TestElement",
+        props: {
+            text: "x",
+        },
+        data: function() {
+            return {
+            }
+        },
+        ready: function() {
+            console.log("### " + this.text + " ready!");
+        },
+    }
+</script>
